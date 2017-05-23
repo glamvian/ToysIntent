03cbc6c732de589c0254d4678112a9af51e278df
glamvian <glmvn.composer@gmail.com>
2017-05-23 23:07:35 +0700

    adding code when click button toas view uoo text

--- app/src/main/java/com/example/glamvian/toysintent/MainActivity.java
+++ app/src/main/java/com/example/glamvian/toysintent/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.glamvian.toysintent;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText editText;
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = (EditText) findViewById(R.id.ed_tx);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = MainActivity.this;
+                String message = "auooo";
+                Toast.makeText(context,message,Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
